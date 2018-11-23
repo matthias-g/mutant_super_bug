43f2210e9859ecefc97dd1436ddb0e7d71ed7d06
Matthias Grundmann <matthias@glasmail.de>
2018-11-23 22:50:24 +0100

    Add mutated code

---
+++ mutated.rb
@@ -0,0 +1,35 @@
+class Parent
+  attr_reader :foo
+
+  def initialize(foo)
+    @foo = foo
+  end
+
+  def one
+    two
+  end
+
+  def two
+    false
+  end
+end
+
+class Child < Parent
+  def one
+    super
+  end
+
+  alias_method :two, :one
+end
+
+RSpec.describe Child do
+  it 'works for foo' do
+    foo = Child.new('foo')
+    expect(foo.one).to eq(true)
+  end
+
+  it 'works for bar' do
+    foo = Child.new('bar')
+    expect(foo.one).to eq(false)
+  end
+end
